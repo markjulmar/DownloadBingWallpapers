377bea4642ff8a9ee279706fb8e454e63eadc70f
Mark Smith <mark@julmar.com>
2024-07-16 18:18:56 -0500

    Update to .NET8 and top-level statements.

--- Program.cs
+++ Program.cs
@@ -1,81 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using System.Text.Json;
 
-namespace DownloadBingWallpapers
+const string bingSite = "https://www.bing.com";
+const string latestWallpapers = $"{bingSite}/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-ww";
+
+using var client = new HttpClient();
+
+var wallpaperJson = await client.GetStringAsync(latestWallpapers);
+var wallpaperList = JsonSerializer.Deserialize<Wallpapers>(wallpaperJson, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+if (wallpaperList is null or { Images.Count: 0 })
 {
-    [JsonObject]
-    public class BingWallpaperImage
+    Console.WriteLine("No wallpapers found.");
+    return;
+}
+
+for (var i = 0; i < wallpaperList.Images?.Count; i++)
+{
+    var item = wallpaperList.Images[i];
+    try
     {
-        [JsonProperty("startdate")]
-        public string startDate { get; set; }
-        [JsonProperty("enddate")]
-        public string endDate { get; set; }
-        [JsonProperty("url")]
-        public string Url { get; set; }
-        public string copyright { get; set; }
-        public string title { get; set; }
+        if (item.Url != null)
+        {
+            await DownloadWallpaperAsync(client, item.Url, i);
+        }
     }
-
-    public class Wallpapers
+    catch (Exception ex)
     {
-        public List<BingWallpaperImage> images { get; set; }
-    }
-
-    class Program
-    {
-        const string LatestWallpapers = @"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-ww";
-        const string BingSite = @"https://www.bing.com";
-
-        static async Task Main()
-        {
-            HttpClient client = new HttpClient();
-
-            var wallpaperJson = await client.GetStringAsync(LatestWallpapers);
-            var wallpaperList = JsonConvert.DeserializeObject<Wallpapers>(wallpaperJson);
-            for (int i = 0; i < wallpaperList.images.Count; i++)
-            {
-                BingWallpaperImage item = (BingWallpaperImage)wallpaperList.images[i];
-                try
-                {
-                    await DownloadWallpaperAsync(client, item.Url, i);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Failed to download {item.Url}: {ex.Message}");
-                }
-            }
-        }
-
-        private static async Task DownloadWallpaperAsync(HttpClient client, string url, int pos)
-        {
-            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), GetFilename(url, pos));
-
-            var stream = await client.GetStreamAsync(BingSite + url);
-            using var writer = File.OpenWrite(filename);
-            await stream.CopyToAsync(writer);
-        }
-
-        private static string GetFilename(string url, int pos)
-        {
-            const string idKey = "?id=";
-
-            // /th?id=OHR.BourgesAerial_ROW9185097510_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
-            int startPos = url.IndexOf(idKey);
-            if (startPos > 0)
-            {
-                startPos += idKey.Length;
-                int endPos = url.IndexOf('&', startPos);
-                if (endPos > 0)
-                {
-                    return url.Substring(startPos, endPos - startPos);
-                }
-            }
-
-            return "wallpaper_" + pos.ToString();
-        }
+        await Console.Error.WriteLineAsync($"Failed to download {item.Url}: {ex.Message}");
     }
 }
+
+return;
+
+static async Task DownloadWallpaperAsync(HttpClient client, string url, int pos)
+{
+    var filename = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+        GetFilename(url, pos));
+
+    var stream = await client.GetStreamAsync(bingSite + url);
+    await using var writer = File.OpenWrite(filename);
+    await stream.CopyToAsync(writer);
+}
+
+static string GetFilename(string url, int pos)
+{
+    const string idKey = "?id=";
+
+    // /th?id=OHR.BourgesAerial_ROW9185097510_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
+    var startPos = url.IndexOf(idKey, StringComparison.Ordinal);
+    if (startPos > 0)
+    {
+        startPos += idKey.Length;
+        var endPos = url.IndexOf('&', startPos);
+        if (endPos > 0)
+        {
+            return url[startPos..endPos];
+        }
+    }
+
+    return "wallpaper_" + pos;
+}
+
+public sealed class BingWallpaperImage
+{
+    //public string? Title { get; set; }
+    //public string? Copyright { get; set; }
+    public string? Url { get; set; }
+}
+
+public sealed class Wallpapers
+{
+    public List<BingWallpaperImage>? Images { get; set; }
+}
+
